63e90932a1c1d4a1eb2ca660a2309e6adfa4b772
Excellent88 <82727062+Excellent88@users.noreply.github.com>
2021-10-10 16:11:30 +0300

    "init commit"

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
@@ -12,7 +13,13 @@ func main() {
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create/", createSnippet)
 
-	log.Println("Запуск сервера на порту :4000")
-	err := http.ListenAndServe(":4000", mux) //addr = "host:port"
+	fileServer := http.FileServer(http.Dir("./ui/static"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	addr := flag.String("addr", ":4000", "Сетевой адрес HTTP")
+	flag.Parse()
+
+	log.Printf("Connecting address is: %s", *addr)
+	err := http.ListenAndServe(*addr, mux) //addr = "host:port"
 	log.Fatal(err)
 }
